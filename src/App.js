3a9756e6fbb88fbd66280de938a5e926f5c0914e
Anton P <anton@polishko.win>
2018-08-06 19:59:00 -0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,70 @@
 import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+
 import logo from './logo.svg';
 import './App.css';
 
+// Текст вебсайта
+const siteLogoText = 'Стартер+';
+
+const PageNavigation = () => {
+  return <div />;
+};
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">{siteLogoText}</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="#">Главная</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#services">Услуги</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#contacts">Контакты</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#map">Как к нам проехать</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#about">О Нас</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
       </div>
     );
   }
